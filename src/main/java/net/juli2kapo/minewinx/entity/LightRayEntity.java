2920ce4b46866057d7c9e23893b05535490c89df
brezza <brezza@dc.uba.ar>
2025-09-19 10:23:27 -0300

    FIXED SOME PLAYER ILLUSION STUFF, MISSING ADDING MOBS AND FIXING THEIR ATTACK PROBLEMS

---
+++ src/main/java/net/juli2kapo/minewinx/entity/LightRayEntity.java
@@ -0,0 +1,6 @@
+package net.juli2kapo.minewinx.entity;
+
+import net.minecraft.world.entity.Entity;
+
+public class LightRayEntity extends Entity {
+}
---
+++ src/main/java/net/juli2kapo/minewinx/entity/client/model/LightRayModel.java
@@ -0,0 +1,54 @@
+// Made with Blockbench 4.12.6
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+package net.juli2kapo.minewinx.entity.client.model;
+
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.juli2kapo.minewinx.MineWinx;
+import net.juli2kapo.minewinx.entity.LightRayEntity;
+import net.juli2kapo.minewinx.entity.SpeakerEntity;
+import net.juli2kapo.minewinx.entity.client.animations.SpeakerAnimations;
+import net.minecraft.client.model.HierarchicalModel;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
+public class LightRayModel<T extends LightRayEntity> extends HierarchicalModel<T> {
+
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "lightraymodel"), "main");
+	private final ModelPart bb_main;
+
+	public LightRayModel(ModelPart root) {
+		this.bb_main = root.getChild("bb_main");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, -2.0F, -3.0F, 2.0F, 2.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		return LayerDefinition.create(meshdefinition, 16, 16);
+	}
+
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+		bb_main.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+
+	@Override
+	public ModelPart root() {
+		return null;
+	}
+
+	@Override
+	public void setupAnim(T pEntity, float pLimbSwing, float pLimbSwingAmount, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch) {
+
+	}
+}
